50504809fa6b8f1e60da90d10fca050136495f40
pctrainor <trainorp14@gmail.com>
2024-12-22 15:34:42 -0600

    react profile

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import qrCode from './qrcode_pctrainor.github.io.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      <header className="App-header">
+        <a href="https://data-phil.com" target="_blank" rel="noopener noreferrer">
+          <img src={qrCode} className="App-logo" alt="logo" />
+        </a><br/><br/>
+        <h1>Phillip C. Trainor <br/><br/>PMP | Naval Aviator <br/> Veteran | Leader <br/><br/><a href="mailto:ptrainor@uark.edu" className="mailto-link">ptrainor@uark.edu</a></h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
