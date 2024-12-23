722dedd6ecdda863b5323aa860a5e91e7a747a40
Phil <ptrainor@uark.edu>
2024-12-22 18:42:07 -0600

    update layout.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ function App() {
   return (
     <div className="App"> 
       <header className="App-header">
-        <a href="https://data-phil.com" target="_blank" rel="noopener noreferrer">
+        <h1>
+        Phillip C. Trainor</h1><br/>
+        <a href="https://pctrainor.github.io/my_personal_site/" target="_blank" rel="noopener noreferrer">
           <img src={qrCode} className="App-logo" alt="logo"  title='click for full portfolio'/>
         </a>
-        <h1>
-        <br/> Phillip C. Trainor <br/>
-          <p>PMP | Naval Aviator <br/>
-          Veteran | Leader </p>
-          
-          <a href="mailto:ptrainor@uark.edu" className="mailto-link">ptrainor@uark.edu</a>
+        <br/><br/>
+        <figcaption>Click the QR code to visit my full portfolio</figcaption>
+        <h1><p>PMP | Naval Aviator
+          <br/>Veteran | Leader</p>
+          <p><a href="mailto:ptrainor@uark.edu" className="mailto-link">ptrainor@uark.edu</a></p>
         </h1>
       </header>
     </div>
