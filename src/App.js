0d7e7b83e03cd33bf579f9db3cedbfc27b280f17
pctrainor <trainorp14@gmail.com>
2025-01-06 11:33:53 -0600

    added Contact.js component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import qrCode from './qrcode_pctrainor.github.io.png';
 import './App.css';
+import Contact from './Contact'; // Import Contact component
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
       <header className="App-header">
-        <h1>
-        Phillip C. Trainor</h1><br/>
-        <a href="https://pctrainor.github.io/my_personal_site/" target="_blank" rel="noopener noreferrer">
-          <img src={qrCode} className="App-logo" alt="logo"  title='click for full portfolio'/>
+        <h1>Phillip C. Trainor</h1>
+        <div className="info-container"> {/* Added a container for better spacing */}
+          <p>PMP | Naval Aviator | Veteran | Leader</p> 
+          <br /><br />        
+          <a
+          href="https://pctrainor.github.io/my_personal_site/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={qrCode}
+            className="App-logo"
+            alt="logo"
+            title="click for full portfolio"
+          />
         </a>
-        <br/><br/>
-        <figcaption>Click the QR code to visit my full portfolio</figcaption>
-        <h1><p>PMP | Naval Aviator
-          <br/>Veteran | Leader</p>
-          <p><a href="mailto:ptrainor@uark.edu" className="mailto-link">ptrainor@uark.edu</a></p>
-        </h1>
+        <br /><br /><br /> 
+        <figcaption>Click/Scan the QR code above to visit my full portfolio!</figcaption>
+          <Contact /> {/* Contact component placed here */}
+        </div>
       </header>
     </div>
   );
---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+return (
+<div className="contact">
+    <h3>Contact Me:</h3>
+    <p>
+    <a href="mailto:ptrainor@uark.edu" className="mailto-link" target="_blank"
+          rel="noopener noreferrer">
+        ptrainor@uark.edu
+    </a>
+    </p>
+</div>
+);
+}
+
+export default Contact;
